1dd7090f77defec73b87ff779c216f26ebe4cfa6
theo.lillieskold@gmail.com <theo.lillieskold@gmail.com>
2025-09-18 11:32:02 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿namespace CircleArea
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle circle1 = new Circle();
+            Circle circle2 = new Circle();
+
+            circle1.radius = 5;
+            circle2.radius = 6;
+
+            Console.WriteLine(circle1.GetArea(circle1.radius));
+            Console.WriteLine(circle2.GetArea(circle2.radius));
+        }
+    }
+
+    class Circle
+    {
+        public int radius;
+
+        public double GetArea(int radius)
+        {
+            double area = radius * radius * Math.PI;
+            return area;
+        }
+    }
+}
